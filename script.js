d6302acd5396ea499dfe1bd3506e97d02d657343
KhaldJallad <khaled.mjallad@gmail.com>
2024-08-20 22:24:31 +0300

    add new transtion and add new script for nav bar

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
     $(".navItem").on('click', function(){
         let navItem = $(this).attr('id');
-        $(navItem).attr('class', 'active navItem nav-link text-light')
-    })
+        $(".navItem").attr('class', "navItem nav-link text-light");
+        $("#"+navItem).attr('class', 'active navItem nav-link text-light');
+    });
 
 })
