55167fc8d94755735ae52bcd9e636a88f7b5dfac
KhaldJallad <khaled.mjallad@gmail.com>
2024-08-21 22:57:19 +0300

    add more css to make the site better

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 $(document).ready(function(){
-    $(".navItem").on('click', function(){
-        let navItem = $(this).attr('id');
-        $(".navItem").attr('class', "navItem nav-link text-light");
-        $("#"+navItem).attr('class', 'active navItem nav-link text-light');
+AOS.init();
+$(".navItem").on('click', function(){
+let navItem = $(this).attr('id');
+$(".navItem").attr('class', "navItem nav-link text-light");
+$("#"+navItem).attr('class', 'active navItem nav-link text-light');
+});
+
+var sectionIds = $('a.nav-link');
+$(document).scroll(function(){
+sectionIds.each(function(){
+
+var container = $(this).attr('href');
+
+if(scrollPosition < containerBottom - 20 && scrollPosition>= containerOffset - 20){
+    $(this).addClass('active');
+    } else{
+    $(this).removeClass('active');
+    }
+
+
+    });
     });
 
-})
+
+
+
+    })
